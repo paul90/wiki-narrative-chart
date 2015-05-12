1e19f79106fa0671ced639fbe8755ea655273723
Paul Rodwell <paul.rodwell@btinternet.com>
2015-05-12 11:02:21 +0100

    update dependencies
    but not too unto date, as more recent clojurescript requires a beta clojure version...

--- project.clj
+++ project.clj
@@ -2,12 +2,11 @@
   :description "Federated Wiki Narrative Chart: explore a page's twins in a federated wiki neighbourhood, and how they related."
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8"]
-                 [cljs-http "0.1.27"]
-                 [com.cognitect/transit-cljs "0.8.205"]
-                 [cuerdas "0.3.0"]]
+                 [cljs-http "0.1.30"]
+                 [cuerdas "0.3.2"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
